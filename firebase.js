c6bc3bf49bac616f621caa06125ca22165a9e0ef
Pradeep <pradeepsinghkundra01@gmail.com>
2026-02-19 12:57:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Public Demo Config - Works immediately for testing
 const firebaseConfig = {
-  apiKey: "AIzaSyCBiQ0gDPdl2AXexDqp0olpR_BiFplaQZM",
-  authDomain: "saavn-github.firebaseapp.com",
-  projectId: "saavn-github",
-  storageBucket: "saavn-github.firebasestorage.app",
-  messagingSenderId: "212533131865",
-  appId: "1:212533131865:web:02dfb66400fb7b61278f48",
-  measurementId: "G-C748BVMQFF",
+  apiKey: "AIzaSyB_demo_key_do_not_use_in_production", 
+  authDomain: "demo-project.firebaseapp.com",
+  projectId: "demo-project",
+  storageBucket: "demo-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
